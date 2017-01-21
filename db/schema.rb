93eb9d8fe546bdd20a1d842b32d832736e3f4ae6
shlok007 <shlok.srivastava15@gmail.com>
2017-01-21 15:25:07 +0530

    fix heroku internal server error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109202905) do
+ActiveRecord::Schema.define(version: 20170110075953) do
 
   create_table "design_events", force: :cascade do |t|
     t.string   "title"
